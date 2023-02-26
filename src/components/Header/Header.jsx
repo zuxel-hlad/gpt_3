073c166160e61668114f72402c2a914efa47a4ab
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2023-02-27 01:07:17 +0200

    fixed app container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,66 +16,68 @@ const Header = () => {
                 <span></span>
                 <span></span>
             </button>
-            <div className="header__container container">
-                <a href="#" className="header__logo">
-                    <img src={logo} alt="GPT-3 logotype" />
-                </a>
-                <nav
-                    onClick={() => setIsBurgerOpen(false)}
-                    className={`header__nav${
-                        isBurgerOpen ? ' header__nav_active' : ''
-                    }`}
-                >
-                    <ul
-                        className={`header__list${
-                            isBurgerOpen ? ' header__list_active' : ''
+            <div className="container">
+                <div className="header__container">
+                    <a href="#" className="header__logo">
+                        <img src={logo} alt="GPT-3 logotype" />
+                    </a>
+                    <nav
+                        onClick={() => setIsBurgerOpen(false)}
+                        className={`header__nav${
+                            isBurgerOpen ? ' header__nav_active' : ''
                         }`}
-                        onClick={e => e.stopPropagation()}
                     >
-                        <button
-                            className="header__list-close"
-                            onClick={() => setIsBurgerOpen(false)}
+                        <ul
+                            className={`header__list${
+                                isBurgerOpen ? ' header__list_active' : ''
+                            }`}
+                            onClick={e => e.stopPropagation()}
                         >
-                            <span></span>
-                            <span></span>
-                        </button>
-                        <li className="header__list-item">
-                            <a href="#" className="header__list-link">
-                                Home
-                            </a>
-                        </li>
-                        <li className="header__list-item">
-                            <a href="#" className="header__list-link">
-                                What is GPT?
-                            </a>
-                        </li>
-                        <li className="header__list-item">
-                            <a href="#" className="header__list-link">
-                                Open AI
-                            </a>
-                        </li>
-                        <li className="header__list-item">
-                            <a href="#" className="header__list-link">
-                                Case Studies
-                            </a>
-                        </li>
-                        <li className="header__list-item">
-                            <a href="#" className="header__list-link">
-                                Library
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="header__login">
-                    {/* <button type="button" className="header__login-btn">
+                            <button
+                                className="header__list-close"
+                                onClick={() => setIsBurgerOpen(false)}
+                            >
+                                <span></span>
+                                <span></span>
+                            </button>
+                            <li className="header__list-item">
+                                <a href="#" className="header__list-link">
+                                    Home
+                                </a>
+                            </li>
+                            <li className="header__list-item">
+                                <a href="#" className="header__list-link">
+                                    What is GPT?
+                                </a>
+                            </li>
+                            <li className="header__list-item">
+                                <a href="#" className="header__list-link">
+                                    Open AI
+                                </a>
+                            </li>
+                            <li className="header__list-item">
+                                <a href="#" className="header__list-link">
+                                    Case Studies
+                                </a>
+                            </li>
+                            <li className="header__list-item">
+                                <a href="#" className="header__list-link">
+                                    Library
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="header__login">
+                        {/* <button type="button" className="header__login-btn">
                         Sign in
                     </button> */}
-                    <button
-                        type="button"
-                        className="header__login-btn header__login-btn_logged"
-                    >
-                        Sign in
-                    </button>
+                        <button
+                            type="button"
+                            className="header__login-btn header__login-btn_logged"
+                        >
+                            Sign in
+                        </button>
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,40 +5,43 @@ import './hero.scss';
 const Hero = () => {
     return (
         <section className="hero">
-            <div className="hero__text">
-                <h1 className="title title_gradient hero__title">
-                    Let’s Build Something amazing with GPT-3 OpenAI
-                </h1>
-                <p className="hero__descr">
-                    Yet bed any for travelling assistance indulgence unpleasing.
-                    Not thoughts all exercise blessing. Indulgence way
-                    everything joy alteration boisterous the attachment. Party
-                    we years to order allow asked of.
-                </p>
-                <form className="hero__form">
-                    <input
-                        className="hero__form-input"
-                        type="email"
-                        placeholder="Your Email Address"
-                    />
-                    <button className="hero__form-btn" type="submit">
-                        Get Started
-                    </button>
-                </form>
-                <div className="hero__peoples">
-                    <div className="hero__peoples-image">
-                        <img
-                            src={peoples}
-                            alt="1,600 people requested access a visit in last 24 hours"
+            <div className="container hero__wrapper">
+                <div className="hero__text">
+                    <h1 className="title title_gradient hero__title">
+                        Let’s Build Something amazing with GPT-3 OpenAI
+                    </h1>
+                    <p className="hero__descr">
+                        Yet bed any for travelling assistance indulgence
+                        unpleasing. Not thoughts all exercise blessing.
+                        Indulgence way everything joy alteration boisterous the
+                        attachment. Party we years to order allow asked of.
+                    </p>
+                    <form className="hero__form">
+                        <input
+                            className="hero__form-input"
+                            type="email"
+                            placeholder="Your Email Address"
                         />
+                        <button className="hero__form-btn" type="submit">
+                            Get Started
+                        </button>
+                    </form>
+                    <div className="hero__peoples">
+                        <div className="hero__peoples-image">
+                            <img
+                                src={peoples}
+                                alt="1,600 people requested access a visit in last 24 hours"
+                            />
+                        </div>
+                        <span className="hero__peoples-descr">
+                            1,600 people requested access a visit in last 24
+                            hours
+                        </span>
                     </div>
-                    <span className="hero__peoples-descr">
-                        1,600 people requested access a visit in last 24 hours
-                    </span>
                 </div>
-            </div>
-            <div className="hero__image">
-                <img src={heroImage} alt="hero image" />
+                <div className="hero__image">
+                    <img src={heroImage} alt="GPT 3" />
+                </div>
             </div>
         </section>
     );
